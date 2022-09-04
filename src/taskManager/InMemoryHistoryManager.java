8226166c84a8ec108cc97313b11d5f06ec9ab28e
Seva144 <se12va@yandex.ru>
2022-09-05 00:05:07 +0300

    сделал его как вложенный класс

--- src/taskManager/InMemoryHistoryManager.java
+++ src/taskManager/InMemoryHistoryManager.java
@@ -8,68 +8,68 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class InMemoryHistoryManager<T> implements HistoryManager {
+public class InMemoryHistoryManager implements HistoryManager {
+
+    static class CustomLinkedList{
+
+        private static final List<Node> linkedList = new ArrayList<>();
+        private static final HashMap<Integer, Node> searchNode = new HashMap<>();
+
+        private static Node head;
+
+
+        public static void linkList(Node node) {
+            if (head == null) {
+                head = node;
+            } else {
+                head.next=node;
+            }
+            node.prev = head;
+            getTask(node);
+        }
+
+        public static void getTask(Node node) {
+            linkedList.add(node);
+        }
+
+        public static void removeNode(Node node1) {
+            if (searchNode.containsKey(node1.task.getId())) {
+                Node node = searchNode.get(node1.task.getId());
+                if (node.prev != null && node.next != null) {
+                    node.prev.next = node.next;
+                    node.next.prev = node.prev;
+                } else if (node.prev != null) {
+                    head = null;
+                }
+                node.prev = null;
+                node.next = null;
+                linkedList.remove(node);
+            }
+        }
+    }
 
     List<Task> history = new ArrayList<>();
-    private static final HashMap<Integer, Node> searchNode = new HashMap<>();
-
-    //реализация двусвязного списка
-    private final static List<Node> customLinkedList = new ArrayList<>();
-    private Node head;
-    private Node tail;
-
-    public void linkList(Node node) {
-        if (head == null) {
-            head = node;
-        } else {
-            head.next=node;
-        }
-        node.prev = head;
-        tail = node;
-        getTask(node);
-    }
-
-    public void getTask(Node node) {
-        customLinkedList.add(node);
-    }
-
-    public void removeNode(Node node1) {
-        if (searchNode.containsKey(node1.task.getId())) {
-            Node node = searchNode.get(node1.task.getId());
-            if (node.prev != null && node.next != null) {
-                node.prev.next = node.next;
-                node.next.prev = node.prev;
-            } else if (node.prev != null) {
-                head = null;
-            }
-            node.prev = null;
-            node.next = null;
-            customLinkedList.remove(node);
-        }
-    }
-    //********************************
 
     @Override
     public void remove(int id) {
-        removeNode(searchNode.get(id));
-        searchNode.remove(id);
+        CustomLinkedList.removeNode(CustomLinkedList.searchNode.get(id));
+        CustomLinkedList.searchNode.remove(id);
     }
 
     @Override
     public List<Task> getHistory() {
-        for (Node linkList : customLinkedList) {
+        for (Node linkList : CustomLinkedList.linkedList) {
             history.add(linkList.task);
         }
         return history;
     }
 
-
     @Override
     public void add(Task task) {
         Node node = new Node(task);
-        removeNode(node);
-        linkList(node);
-        searchNode.put(task.getId(), node);
+        CustomLinkedList.removeNode(node);
+        CustomLinkedList.linkList(node);
+        CustomLinkedList.searchNode.put(task.getId(), node);
     }
 }
 
